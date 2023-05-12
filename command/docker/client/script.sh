fb1ce2a28d1a35b102e80cc89533c0ccd0e90b93
timzaak <zsy.evan@gmail.com>
2023-05-12 13:32:04 +0800

    fix cicd step2

---
+++ command/docker/client/script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# this runs in Docker to build 
+
+mkdir /protoc && cd /protoc
+
+architecture=$(uname -m)
+if [ $architecture=='aarch64' ];
+then
+    architecture='aarch_64'
+fi
+
+wget https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protoc-21.9-linux-$architecture.zip && unzip protoc-21.9-linux-$architecture.zip && cp bin/* /usr/bin/
+
+cp -r /protoc/include/* /source/protobuf/
