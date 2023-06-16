88a4e9707e0eb998ea044912421bd0ecfb9d7d6a
timzaak <zsy.evan@gmail.com>
2023-06-16 01:22:41 +0000

    update rmqtt version

--- command/docker/mqtt/run.sh
+++ command/docker/mqtt/run.sh
@@ -2,5 +2,5 @@
 
 # 1883(mqtt) 6060(http) 5363(grpc)
 # docker rm -f mqtt
-docker run -d --name mqtt --network=host -v $(pwd)/log:/var/log/rmqtt -v $(pwd)/config/rmqtt.toml:/app/rmqtt/rmqtt.toml -v $(pwd)/config/plugin:/app/rmqtt/plugin rmqtt/rmqtt:0.2.11
+docker run -d --name mqtt --network=host -v $(pwd)/log:/var/log/rmqtt -v $(pwd)/config/rmqtt.toml:/app/rmqtt/rmqtt.toml -v $(pwd)/config/plugin:/app/rmqtt/plugin rmqtt/rmqtt:latest
 # docker logs -f --tail 50 mqtt
