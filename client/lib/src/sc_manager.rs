e4e42bbe05c0d044de917a657da702a5d8844cc5
timzaak <zsy.evan@gmail.com>
2023-06-27 19:38:13 +0800

    bak it

--- client/lib/src/sc_manager.rs
+++ client/lib/src/sc_manager.rs
@@ -1,9 +1,13 @@
 use std::sync::Arc;
 use std::time::Duration;
+use mqrstt::{AsyncEventHandler, ConnectOptions, MqttClient, new_tokio};
+use mqrstt::packets::Packet;
 
 use paho_mqtt as mqtt;
 use prost::Message;
 use tokio::sync::mpsc::Sender;
+use tokio_rustls::rustls;
+use tokio_rustls::rustls::ClientConfig;
 use tokio_stream::StreamExt;
 use crate::config::{NodeInfo, Config as AppConfig};
 
@@ -17,17 +21,121 @@ use crate::server_manager::ServerMessage;
 struct Duplication {
     wr_config: Option<WrConfig>,
     status: Option<NodeStatus>,
+
 }
+
+struct MqttWrapper {
+    pub client:MqttClient,
+    pub client_topic: String,
+    pub network_topic: String,
+    pub deduplication: Duplication,
+    pub sender: Sender<ServerMessage>
+}
+
+impl AsyncEventHandler for MqttWrapper {
+    async fn handle(&mut self, event: Packet) {
+        match event {
+            Packet::Publish(p) => {
+                match &p.topic {
+                    topic if topic == &self.client_topic => {
+                        if let Ok(client_message) = ClientMessage::decode(p.payload) {
+                            if let Some(info) = client_message.info {
+                                match info {
+                                    Config(wr_config) => {
+                                        if self.deduplication.wr_config == Some(wr_config.clone()) {
+                                            return;
+                                        }
+
+                                        let _ = self.sender.send(ServerMessage::SyncConfig(wr_config.clone())).await;
+                                        self.deduplication.wr_config = Some(wr_config);
+                                    }
+                                    Status(status) => {
+                                        if let Some(node_status) = NodeStatus::from_i32(status) {
+                                            if self.deduplication.status == Some(node_status) {
+                                                return;
+                                            }
+                                            match node_status {
+                                                NodeStatus::NodeForbid => {
+                                                    let _ = self.sender.send(
+                                                        ServerMessage::StopWR("node has been forbid or delete".to_owned())
+                                                    ).await;
+                                                    self.deduplication.wr_config = None;
+                                                }
+                                                _ => {
+                                                    // this would conflict with Info::Config message, so ignore this.
+                                                }
+                                            }
+                                            self.deduplication.status = Some(node_status)
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            tracing::warn!("client message can not decode, may should update software");
+                        }
+                    }
+                    topic if topic == &self.network_topic => {
+                        if let Ok(network_message) = NetworkMessage::decode(p.payload) {
+                            if let Some(info) = network_message.info {
+                                match info {
+                                    Peer(peer_change) => {
+                                        let _ = self.sender.send(ServerMessage::SyncPeers(peer_change)).await;
+                                    }
+                                    NStatus(status) => {
+                                        if let Some(NetworkStatus::NetworkDelete) = NetworkStatus::from_i32(status) {
+                                            let _ = self.sender.send(
+                                                ServerMessage::StopWR("network has been delete".to_owned())
+                                            ).await;
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            tracing::warn!("network message can not decode, may should update software");
+                        }
+                    }
+                    _ => {
+                        tracing::warn!("topic:{} message can not decode, may should update software", msg.topic());
+                    }
+                }
+            }
+            Packet::ConnAck(_) => {
+                tracing::info!("mqtt connected");
+            }
+            _ => {}
+        }
+
+    }
+}
+
+
 
 pub struct SCManager {
     sender: Sender<ServerMessage>,
 }
-
 impl SCManager {
     pub fn new(sender: Sender<ServerMessage>) -> Self {
         SCManager {
             sender,
         }
+    }
+
+    async fn mqtt2_reconnect(sender:Sender<ServerMessage>,node_info: &NodeInfo, config:Arc<AppConfig>, deduplication:&mut Duplication) -> anyhow::Result<()> {
+        let client_id = config.identity.pk_base64.clone();
+        let options = ConnectOptions::new(client_id);
+        let (mut network, client) = new_tokio(options);
+        if node_info.mqtt_url.starts_with("mqtts") {
+            let root_certs = rustls::RootCertStore::empty();
+            let config = ClientConfig::builder().with_safe_defaults().with_root_certificates(root_certs).with_no_client_auth();
+            let connector = tokio_rustls::TlsConnector::from(Arc::new(config));
+
+            let stream = tokio::net::TcpStream::connect(node_info.mqtt_url.clone()).await?;
+            let connection = connector.connect(domain, stream).await?;
+            
+            //network.connect(connection,)
+        }
+        Ok(())
+
     }
 
     async fn mqtt_reconnect(sender:Sender<ServerMessage>, node_info: &NodeInfo, config:Arc<AppConfig>, deduplication:&mut Duplication) -> anyhow::Result<()> {
