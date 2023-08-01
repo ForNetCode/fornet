689717820d15081074360e7912dc82685a8014c8
timzaak <zsy.evan@gmail.com>
2023-08-01 18:44:26 +0800

    bak it

--- client/lib/src/server_api/unix.rs
+++ client/lib/src/server_api/unix.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use anyhow::Context;
+use cfg_if::cfg_if;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, Lines};
 use tokio::net::{UnixListener, UnixStream};
 use tokio::task::JoinHandle;
@@ -7,11 +8,24 @@ use tokio::task::JoinHandle;
 pub type APISocket = UnixStream;
 pub type StreamResponse = Lines<BufReader<UnixStream>>;
 
-pub const SERVER_API_SOCKET: &str =  "/var/run/fornet.sock";
+// pub const SERVER_API_SOCKET: &str =  "/var/run/fornet.sock";
 
+cfg_if!{
+    if #[cfg(target_os = "android")] {
+        pub fn get_server_api_socket_path2(dir:&str) -> PathBuf {
+            PathBuf::from(dir).join("fornet_socket.sock")
+        }
 
-pub fn init_api_server(sender: tokio::sync::mpsc::Sender<APISocket>) -> anyhow::Result<JoinHandle<()>> {
-    let api_sock_path = PathBuf::from(SERVER_API_SOCKET);
+    } else {
+        pub fn get_server_api_socket_path1()-> PathBuf {
+            PathBuf::from("/var/run/fornet.sock")
+        }
+
+    }
+}
+
+pub fn init_api_server(sender: tokio::sync::mpsc::Sender<APISocket>, api_socket_path:PathBuf) -> anyhow::Result<JoinHandle<()>> {
+    let api_sock_path = PathBuf::from(api_socket_path);
     if api_sock_path.exists() {
         std::fs::remove_file(&api_sock_path).with_context(||format!("remove api socket fail: {}", api_sock_path.display()))?;
     }
@@ -27,19 +41,33 @@ pub fn init_api_server(sender: tokio::sync::mpsc::Sender<APISocket>) -> anyhow::
     Ok(task)
 }
 
-pub async fn send_command(command: &str) -> anyhow::Result<String> {
-    let mut stream = UnixStream::connect(SERVER_API_SOCKET).await?;
-    stream.write(command.as_bytes()).await?;
-    let mut lines = BufReader::new(stream).lines();
-    while let Some(line) = lines.next_line().await? {
-        return Ok(line);
-    }
-    anyhow::bail!("could not get response");
+#[derive(Debug)]
+pub struct ApiClient {
+    api_socket_path: PathBuf
 }
 
-pub async fn send_command_stream(command:&str) -> anyhow::Result<StreamResponse> {
-    let mut stream = UnixStream::connect(SERVER_API_SOCKET).await?;
-    stream.write(command.as_bytes()).await?;
-    let lines = BufReader::new(stream).lines();
-    Ok(lines)
+impl ApiClient {
+    pub fn new(path:PathBuf) -> Self {
+        Self {
+            api_socket_path: path
+        }
+    }
+
+    pub async fn send_command(&self, command: &str) -> anyhow::Result<String> {
+        let mut stream = UnixStream::connect(&self.api_socket_path).await?;
+        stream.write(command.as_bytes()).await?;
+        let mut lines = BufReader::new(stream).lines();
+        while let Some(line) = lines.next_line().await? {
+            return Ok(line);
+        }
+        anyhow::bail!("could not get response");
+    }
+
+    pub async fn send_command_stream(&self, command:&str) -> anyhow::Result<StreamResponse> {
+        let mut stream = UnixStream::connect(&self.api_socket_path).await?;
+        stream.write(command.as_bytes()).await?;
+        let lines = BufReader::new(stream).lines();
+        Ok(lines)
+    }
 }
+
--- client/lib/src/server_api/windows.rs
+++ client/lib/src/server_api/windows.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use anyhow::{anyhow, bail};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, Lines};
 use tokio::net::windows::named_pipe::{NamedPipeServer, ClientOptions, ServerOptions, NamedPipeClient};
@@ -9,12 +9,15 @@ pub type APISocket = NamedPipeServer;
 pub type StreamResponse = Lines<BufReader<NamedPipeClient>>;
 
 
-pub const SERVER_API_SOCKET: &str = r"\\.\pipe\fornet_sock";
 
-pub fn init_api_server(sender: tokio::sync::mpsc::Sender<APISocket>) -> anyhow::Result<JoinHandle<()>> {
+pub fn get_server_api_socket_path()-> PathBuf {
+    PathBuf::from(r"\\.\pipe\fornet_sock")
+}
+
+pub fn init_api_server(sender: tokio::sync::mpsc::Sender<APISocket>, api_socket_path:PathBuf) -> anyhow::Result<JoinHandle<()>> {
     let mut server = ServerOptions::new()
         .first_pipe_instance(true)
-        .create(SERVER_API_SOCKET)?;
+        .create(api_socket_path)?;
     tracing::info!("api server open");
     let task: JoinHandle<()> = tokio::spawn(async move {
         //TODO: test it, fix it
@@ -29,39 +32,51 @@ pub fn init_api_server(sender: tokio::sync::mpsc::Sender<APISocket>) -> anyhow::
     Ok(task)
 }
 
-pub async fn send_command(command: &str) -> anyhow::Result<String> {
-    let mut client = ClientOptions::new()
-        .open(SERVER_API_SOCKET)?;
+#[derive(Debug)]
+pub struct ApiClient {
+    api_socket_path: PathBuf
+}
+impl ApiClient {
+    pub fn new(path:PathBuf) -> Self {
+        Self {
+            api_socket_path: path
+        }
+    }
 
-    client.write(command.as_bytes()).await?;
-    client.readable().await?;
+    pub async fn send_command(&self, command: &str) -> anyhow::Result<String> {
+        let mut client = ClientOptions::new()
+            .open(&self.api_socket_path)?;
 
-    let mut buf = vec![0; 4096];
-    loop {
-        match client.try_read(&mut buf) {
-            Ok(size) => {
-                if size > 0 {
-                    let result = String::from_utf8(buf[0..size].to_vec())?;
-                    return Ok(result);
-                } else {
-                    return Ok("".to_string());
+        client.write(command.as_bytes()).await?;
+        client.readable().await?;
+
+        let mut buf = vec![0; 4096];
+        loop {
+            match client.try_read(&mut buf) {
+                Ok(size) => {
+                    if size > 0 {
+                        let result = String::from_utf8(buf[0..size].to_vec())?;
+                        return Ok(result);
+                    } else {
+                        return Ok("".to_string());
+                    }
                 }
-            }
-            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                continue;
-            }
-            Err(e) => {
-                bail!("fail to send message:{}", e)
+                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    bail!("fail to send message:{}", e)
+                }
             }
         }
     }
-}
 
 
-pub async fn send_command_stream(command:&str) -> anyhow::Result<StreamResponse> {
-    let mut client = ClientOptions::new()
-        .open(SERVER_API_SOCKET)?;
-    client.write(command.as_bytes()).await?;
-    client.readable().await?;
-    Ok(BufReader::new(client).lines())
+    pub async fn send_command_stream(&self, command:&str) -> anyhow::Result<StreamResponse> {
+        let mut client = ClientOptions::new()
+            .open(&self.api_socket_path)?;
+        client.write(command.as_bytes()).await?;
+        client.readable().await?;
+        Ok(BufReader::new(client).lines())
+    }
 }
