591b0f48f7c5677ccbb59d4ca1bfc85b46b4dfb4
timzaak <zsy.evan@gmail.com>
2023-08-08 13:58:08 +0800

    add permission

--- app/android/app/src/main/kotlin/com/fornet/ui/ForNetVPNService.kt
+++ app/android/app/src/main/kotlin/com/fornet/ui/ForNetVPNService.kt
@@ -1,18 +1,23 @@
 package com.fornet.ui
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Intent
 import android.net.VpnService
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.os.ParcelFileDescriptor
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class ForNetVPNService: VpnService() {
     companion object {
-        val TAG = "VPNService"
+        const val TAG = "VPNService"
+        const val NotificationId = 1
     }
-    //lateinit var fileDescriptor: ParcelFileDescriptor
+    //var fileDescriptor: ParcelFileDescriptor = null
     lateinit var notificationManager: NotificationManager
+    lateinit var notificationBuilder: NotificationCompat.Builder
     private val binder = LocalBinder()
 
     inner class LocalBinder : Binder() {
@@ -28,17 +33,38 @@ class ForNetVPNService: VpnService() {
     }
     override fun onCreate() {
         super.onCreate()
-        initNotification()
-        stopForeground(true)
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "ForNet"
+            val channel = NotificationChannel(channelId, "ForNet Service", NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "ForNet Service Channel"
+            notificationManager.createNotificationChannel(channel)
+            channelId
+        } else {
+            ""
+        }
+        notificationBuilder = NotificationCompat.Builder(this, channelId)
+        createNotification()
+    }
+
+    private fun createNotification() {
+        val notification = notificationBuilder
+            .setContentText("ForNet Service Running")
+            .setContentTitle("ForNet")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setOngoing(true)
+            .build()
+        Log.d(TAG, "start notification")
+        startForeground(NotificationId, notification)
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(TAG, "this service: ${Thread.currentThread()}")
+        Log.d(TAG, "this service: ${Thread.currentThread()}, flags:${flags}, startId:${startId}")
         super.onStartCommand(intent, flags, startId)
         return START_NOT_STICKY // don't use sticky (auto restart), the new service (from auto restart) will lose control
     }
-    private fun initNotification() {
-        //TODO: add it
-    }
+
+
 
     fun setupVPN() {
         //val builder = Builder()
@@ -48,6 +74,7 @@ class ForNetVPNService: VpnService() {
     fun destroy() {
         stopSelf()
         Log.d(TAG, "destroy service")
+        stopForeground(true)
     }
 
 
--- app/windows/flutter/generated_plugin_registrant.cc
+++ app/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <system_tray/system_tray_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BitsdojoWindowPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   SystemTrayPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SystemTrayPlugin"));
 }
