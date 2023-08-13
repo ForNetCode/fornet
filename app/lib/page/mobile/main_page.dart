7392d7d2058c27ca4ba26200136dce7450e7e822
timzaak <zsy.evan@gmail.com>
2023-08-13 12:29:03 +0800

    bak

--- app/lib/page/mobile/main_page.dart
+++ app/lib/page/mobile/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:for_net_ui/native/ffi.dart';
 import 'package:for_net_ui/page/mobile/join_network_page.dart';
 import 'package:for_net_ui/state/fornet_lib_manager.dart';
 import 'package:logger/logger.dart';
@@ -12,7 +14,7 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<RuntimeStatus>(
       builder:(_,status,__) {
-        log.i('runtime status: ${status}');
+        log.i('runtime status: $status');
         return Scaffold(
             appBar: AppBar(
               title: const Text('ForNet'),
@@ -29,8 +31,9 @@ class MainPage extends StatelessWidget {
                     style: const ButtonStyle(),
                     onPressed: ()=> {}, child: const Text('Hello World!')),
                 const SizedBox(height: 30,),
-                OutlinedButton.icon(onPressed: () => {
-
+                OutlinedButton.icon(onPressed: () async{
+                  var version = await api.version();
+                  SmartDialog.showToast('ForNetLib Version: $version');
                 }, icon: const Icon(Icons.info), label: const Text('Info'))
               ],
             )
