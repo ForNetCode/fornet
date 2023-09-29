0cb0d956678473fe2f324959405a2b3048264744
timzaak <zsy.evan@gmail.com>
2023-09-29 15:41:34 +0800

    bak

--- windows-build.ps1
+++ windows-build.ps1
@@ -1,7 +1,7 @@
 #set-ExecutionPolicy RemoteSigned
 mkdir protoc
 cd protoc
-wget https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protoc-21.9-win64.zip -outfile "protoc-21.9-win64.zip"
+Invoke-WebRequest  https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protoc-21.9-win64.zip -outfile "protoc-21.9-win64.zip"
 Expand-Archive protoc-21.9-win64.zip -DestinationPath ./
 $env:Path += (";" + $PWD.path + "\bin")
 
@@ -11,7 +11,7 @@ cargo build --release
 cd ../
 mkdir release
 
-wget https://github.com/ForNetCode/simple-windows-tun/releases/download/v0.1.0/fortun-win11-x86_64.zip -outfile "driver.zip"
+Invoke-WebRequest  https://github.com/ForNetCode/simple-windows-tun/releases/download/v0.1.0/fortun-win11-x86_64.zip -outfile "driver.zip"
 Expand-Archive driver.zip -DestinationPath ./driver_tmp
 mkdir driver
 mv .\driver_tmp\fortun\* .\driver\
