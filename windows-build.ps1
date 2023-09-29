ebee647514627fbdc2a8f39c8e7ca23c567ea3de
timzaak <zsy.evan@gmail.com>
2023-09-29 15:22:32 +0800

    fix windows cicd

--- windows-build.ps1
+++ windows-build.ps1
@@ -1,4 +1,4 @@
- #set-ExecutionPolicy RemoteSigned
+#set-ExecutionPolicy RemoteSigned
 mkdir protoc
 cd protoc
 wget https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protoc-21.9-win64.zip -outfile "protoc-21.9-win64.zip"
@@ -11,4 +11,9 @@ cargo build --release
 cd ../
 mkdir release
 
-Compress-Archive -P ./client/target/release/fornet.exe -P ./client/target/release/fornet-cli.exe -DestinationPath ./release/fornet-window64.zip
+wget https://github.com/ForNetCode/simple-windows-tun/releases/download/v0.1.0/fortun-win11-x86_64.zip -outfile "driver.zip"
+Expand-Archive driver.zip -DestinationPath ./driver_tmp
+mkdir driver
+mv .\driver_tmp\fortun\* .\driver\
+
+Compress-Archive -P ./client/target/release/fornet.exe -P ./driver -P ./client/target/release/fornet-cli.exe -DestinationPath ./release/fornet-window64.zip
