11f8af3f2943fcbddb88eab66e2eefdf81d43cdb
Timzaak <zsy.evan@gmail.com>
2023-10-11 22:17:04 +0800

    add production nginx config

--- command/docker/keycloak/dev/run.sh
+++ command/docker/keycloak/dev/run.sh
@@ -2,7 +2,11 @@
 
 docker rm -f fornet-keycloak
 # This is for linux, please change network=host if use others.
+<<<<<<< Updated upstream
 docker run -d --network=host --name=fornet-keycloak \
+=======
+docker run -d --network=host --name=fornet-keycloak\
+>>>>>>> Stashed changes
     -e KC_DB_URL='jdbc:postgresql://127.0.0.1:5432/keycloak' \
     -e KC_DB_USERNAME=keycloak_user \
     -e KC_DB_PASSWORD='keycloak_db_password' \
