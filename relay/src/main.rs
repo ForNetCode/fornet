b92db5a7b654542c1e36125d2e6c3d9292c2e3fc
timzaak <zsy.evan@gmail.com>
2023-10-27 00:24:43 +0800

    add simple ice api

--- relay/src/main.rs
+++ relay/src/main.rs
@@ -3,6 +3,8 @@ mod simple;
 mod reconnect;
 mod simple_stun;
 
+mod simple_ice_api;
+
 
 
 
@@ -10,8 +12,10 @@ mod simple_stun;
 async fn main() -> anyhow::Result<()>{
     tracing_subscriber::fmt::init();
     //simple::simple().await.unwrap();
-    simple_stun::simple_stun().await.unwrap();
+    //simple_stun::simple_stun().await.unwrap();
     //reconnect::reconnect().await.unwrap();
+
+    simple_ice_api::simple_ice_api().await.unwrap();
     tokio::signal::ctrl_c().await?;
     Ok(())
 }
